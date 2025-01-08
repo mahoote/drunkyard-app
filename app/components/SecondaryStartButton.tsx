0b808fead926dbf8e02b750112a8f9274e135fa4
Martin Hoff Teigen <martin@teigen.dev>
2025-01-08 21:07:06 +0100

    Start page (#6)
    * Create background and not found page
    
    * Move index to tabs folder
    
    * Refactor layout
    
    * Set dark mode default
    
    * Create basic button for starting game
    
    * Make simple front page design + add dependencies for svg
    
    * Refactor + app logo
    
    * Create nav bar with front page icons
    
    * Front page done
    
    * Add scroll view
    
    * Refactor layout and ProfileIcon

---
+++ app/components/SecondaryStartButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Pressable, View } from 'react-native'
+import AppText from '@/app/components/AppText'
+import SquaresBackgroundImage from '@/assets/images/squares-background-1.svg'
+
+interface SecondaryStartButtonProps {
+    icon: React.ReactNode
+    title: string
+}
+
+export default function SecondaryStartButton({
+    icon,
+    title,
+}: SecondaryStartButtonProps) {
+    return (
+        <Pressable className="w-full gap-3 justify-center items-center p-4 primary-50 bg-primary-500 rounded-3xl relative overflow-hidden">
+            <View className="absolute inset-0">
+                <SquaresBackgroundImage
+                    width="100%"
+                    height="100%"
+                    preserveAspectRatio="none"
+                    color="#F0F1FD"
+                />
+            </View>
+            {icon}
+            <AppText size="display-xs-bold" color="text-background">
+                {title}
+            </AppText>
+        </Pressable>
+    )
+}
