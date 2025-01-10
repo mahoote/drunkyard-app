2de6b2085231364f910583b13798610e2a762655
mahoote <martin@teigen.biz>
2025-01-10 20:08:13 +0100

    Refactor Overlay.tsx

--- app/components/Overlay.tsx
+++ app/components/Overlay.tsx
@@ -1,9 +1,10 @@
 import { FontAwesome } from '@expo/vector-icons'
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Animated, {
     useSharedValue,
     useAnimatedStyle,
     withTiming,
+    runOnJS,
 } from 'react-native-reanimated'
 import NavButtons from '@/app/components/buttons/NavButtons'
 import AppView from '@/app/components/views/AppView'
@@ -14,17 +15,42 @@ interface OverlayProps {
     children: React.ReactNode
 }
 
+/**
+ * Displays an overlay that covers the entire screen.
+ * Fade in and out when the component is shown or hidden.
+ * @param isVisible
+ * @param toggleVisibility
+ * @param children
+ * @constructor
+ */
 const Overlay = ({ isVisible, toggleVisibility, children }: OverlayProps) => {
-    const opacity = useSharedValue(isVisible ? 1 : 0)
+    const opacity = useSharedValue(0) // Start with invisible state
+    const [shouldRender, setShouldRender] = useState(false)
 
+    /**
+     * Ensure the component is rendered when it's visible.
+     * Animate the opacity when the component is shown or hidden.
+     * When the component is hidden, set the shouldRender state to false after the animation completes.
+     */
     useEffect(() => {
-        opacity.value = withTiming(isVisible ? 1 : 0, { duration: 300 })
+        if (isVisible) {
+            setShouldRender(true)
+            opacity.value = withTiming(1, { duration: 300 })
+        } else {
+            opacity.value = withTiming(0, { duration: 300 }, () => {
+                runOnJS(setShouldRender)(false)
+            })
+        }
     }, [isVisible])
 
     const animatedStyle = useAnimatedStyle(() => ({
         opacity: opacity.value,
-        display: opacity.value === 0 ? 'none' : 'flex',
     }))
+
+    // Don't render the component when it's not visible
+    if (!shouldRender) {
+        return null
+    }
 
     return (
         <Animated.View
@@ -37,7 +63,6 @@ const Overlay = ({ isVisible, toggleVisibility, children }: OverlayProps) => {
                         rightButton={<FontAwesome name="close" size={24} />}
                         rightButtonAction={toggleVisibility}
                     />
-
                     {children}
                 </AppView>
             </AppView>
