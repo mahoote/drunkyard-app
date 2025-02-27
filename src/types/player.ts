5c54158b3bf7ec1ad20553fbb4ce60d4817c01da
mahoote <martin@teigen.biz>
2025-02-27 04:33:20 +0100

    29 Create updatePlayer

---
+++ src/types/player.ts
@@ -0,0 +1,18 @@
+export interface Player {
+    id: string
+    user_id?: string
+    username?: string
+    first_name?: string
+    last_name?: string
+    is_guest: boolean
+    created_at: string
+    updated_at?: string
+    deleted_at?: string
+}
+
+export interface PlayerUpdateDto {
+    user_id: string
+    username?: string
+    first_name?: string
+    last_name?: string
+}
