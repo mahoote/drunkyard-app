92fd72cc91d39f20092d444ee39f2b444a4a90d6
Martin Hoff Teigen <martin@teigen.dev>
2025-03-01 21:24:55 +0100

    hotfix Update app name (#30)

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ import 'dotenv/config'
 
 export default {
     expo: {
-        name: 'dg-app',
-        slug: 'dg-app',
+        name: 'Drunkyard',
+        slug: 'drunkyard-app',
         version: '1.0.0',
         orientation: 'portrait',
         icon: './assets/images/icon.png',
